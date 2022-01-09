86fe5a834a3c7d2f5184715d65ac5460463f5810
Supercoder2311 <84426763+Supercoder2311@users.noreply.github.com>
2022-01-09 10:15:41 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,24 @@
+function logout(){
+window.location="index.html"
+}
+function SOSMO(){
+    window.location="sosmo.html";
+}
+function capLvl1(){
+window.location="cap.html";
+}
+function cascadeLvl2(){
+    window.location="cascade.html";
+}
+function sandLvl3(){
+    window.location="sand.html";
+}
+function lakeLvl4(){
+    window.location="lake.html";
+}
+function woodedLvl5(){
+    window.location="wooded.html";
+}
+function cloudLvl6(){
+    window.location="cloud.html";
+}
